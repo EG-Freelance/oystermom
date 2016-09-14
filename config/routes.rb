d158d322269727058c9e69a8779a6b3948fd6d58
Erik <erik.w.gibbons@gmail.com>
2016-09-14 22:00:39 +0000

    hotfix

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   post '/gallery' => 'media_files#create'
 
   resources :posts
+  resources :media_files
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
