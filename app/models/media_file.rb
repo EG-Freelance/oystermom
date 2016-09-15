4986221fa333f28053845da9096447db025b1ed3
Erik <erik.w.gibbons@gmail.com>
2016-09-15 13:46:58 +0000

    add content_type validation for images

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -2,5 +2,7 @@ class MediaFile < ActiveRecord::Base
   belongs_to :post
   
   has_attached_file :image
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/bmp"] }
+  
   has_attached_file :video
 end
