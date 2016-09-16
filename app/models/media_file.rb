904ba0d8bea440fa1b87dd7e1b3d10e6f9c94d70
Erik <erik.w.gibbons@gmail.com>
2016-09-16 18:31:55 +0000

    hotfix

--- app/models/media_file.rb
+++ app/models/media_file.rb
@@ -7,6 +7,6 @@ class MediaFile < ActiveRecord::Base
   has_attached_file :video, styles: {
       medium: { format: 'mp4' },
       thumb: { geometry: '300x300#', format: 'jpg', time: 10 }
-    }, processors: [:ffmpeg]
+    }, processors: [:transcoder]
   validates :video, :attachment_content_type => { :content_type => /\Avideo\/.*\Z/ }
 end
